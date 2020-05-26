32553fc077346df7901eaa5c44e35031462cd2fc
Anderson Rodrigues <anddersonrds@gmail.com>
2020-05-26 00:24:56 -0300

    add relations between users and appointments

--- src/database/migrations/1589805213031-CreateAppointments.ts
+++ src/database/migrations/1589805213031-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1589805213031
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1590032582302-CreateUsers.ts
+++ src/database/migrations/1590032582302-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1590032582302 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
