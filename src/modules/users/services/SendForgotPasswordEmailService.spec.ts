ed63c6bb9e5eb0475f68144961a46eb5901ece79
Anderson Rodrigues <anddersonrds@gmail.com>
2020-09-25 23:42:31 -0300

    wip

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,20 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+
+    const createUser = new SendForgotPasswordEmailService(fakeUsersRepository);
+
+    // const user = await createUser.execute({
+    //   name: 'Anderson Rodrigues',
+    //   email: 'anddersonrds@gmail.com',
+    //   password: '123456',
+    // });
+
+    expect(user).toHaveProperty('id');
+  });
+});
---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,21 @@
+import { injectable, inject } from 'tsyringe';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute(): Promise<void> {}
+}
+
+export default SendForgotPasswordEmailService;
