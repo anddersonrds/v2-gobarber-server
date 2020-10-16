2337cb139ff7790e7b8b84c14c541fda913df7e4
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-16 20:54:03 -0300

    add template engine

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -7,7 +7,7 @@ import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplate';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
