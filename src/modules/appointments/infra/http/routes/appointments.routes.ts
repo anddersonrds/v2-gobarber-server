8e858dc6b4b3511f6243aee45957bc0c142f05bf
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-25 01:02:25 -0300

    add list schedules

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 
-import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
