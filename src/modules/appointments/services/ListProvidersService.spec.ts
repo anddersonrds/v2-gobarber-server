e38cb90155262cec3242e3beb8519b184efa8378
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-24 21:00:12 -0300

    add lits providers

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,40 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProvidersService: ListProvidersService;
+
+describe('ListProvidersService', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    listProvidersService = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able list the providers', async () => {
+    const userDoe = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const userTre = await fakeUsersRepository.create({
+      name: 'John Trê',
+      email: 'johntre@example.com',
+      password: '123456',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'John Qua',
+      email: 'johnqua@example.com',
+      password: '123456',
+    });
+
+    const providers = await listProvidersService.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([userDoe, userTre]);
+  });
+});
