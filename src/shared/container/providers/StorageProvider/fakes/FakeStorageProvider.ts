1e8ed3d37c2fa49a4403365b77ecaf04a83400bd
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-25 00:06:20 -0300

    fix providers

--- src/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.ts
@@ -1,4 +1,4 @@
-import IStorageProvider from '../models/IStorageProvider';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 class FakeStorageProvider implements IStorageProvider {
   private storage: string[] = [];
